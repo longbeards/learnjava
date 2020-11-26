967d1aab07d8147aef0a87e50696835579121280
Mukund Krishnan <mukund.krishnan@hotmail.com>
2020-11-26 07:56:07 -0500

    Initial commit

---
+++ src/main/java/com/longbeards/restservice/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package com.longbeards.restservice.controller;
+
+import com.longbeards.restservice.entity.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private Logger logger = LoggerFactory.getLogger(GreetingController.class);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.info("Incoming Request");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
